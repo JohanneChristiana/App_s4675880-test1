3d43552e5ee2412668554e2562dab819199bb7e4
Yohan <johannemenez22@gmail.com>
2023-10-15 15:44:47 +1100

    Initial commit

---
+++ app/src/main/java/com/example/app_s4675880/SecondScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.app_s4675880
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondscreen) //Layout Resource
+        
+        
+    }
+}
